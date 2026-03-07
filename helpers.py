40f3f8ccf620373f62e74c89ad6909eaaf0faa98
Hassan <mohamadhassankadori@gmail.com>
2026-03-07 14:51:15 +0300

    fixed database connection

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import os, psycopg2
 
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://postgres:[Hasonberg-123]@db.gnqtismdbgwyyzcxgneg.supabase.co:5432/postgres')
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://postgres:Hasonberg-123@db.gnqtismdbgwyyzcxgneg.supabase.co:5432/postgres')
 
 def get_db():
     conn = psycopg2.connect(DATABASE_URL)
